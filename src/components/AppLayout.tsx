2c80f1c3a55fdbdc8f7414a2b748db263906cac4
matiaslujanw <matiaslujanw@gmail.com>
2026-04-08 10:50:06 -0300

    s

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -37,7 +37,7 @@ const AppLayout = ({ children }: { children: ReactNode }) => {
                 onClick={() => navigate('/participants')}
                 className="rounded-xl h-9"
               >
-                Participantess
+                Participantes
               </Button>
             </nav>
           </div>
